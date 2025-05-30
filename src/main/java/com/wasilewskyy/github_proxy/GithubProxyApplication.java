cf2e0487cf6fe194924e173d884848529e66bf79
wasilewskyy <jakubwasilewski84@gmail.com>
2025-05-30 17:25:55 +0200

    Initial commit

---
+++ src/main/java/com/wasilewskyy/github_proxy/GithubProxyApplication.java
@@ -0,0 +1,13 @@
+package com.wasilewskyy.github_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wasilewskyy/github_proxy/GithubProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasilewskyy.github_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
